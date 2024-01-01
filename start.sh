75108e5ad3b9fc16b4f71c547b1fd16a138234e4
Giulio Cocconi <coccogiulio8@gmail.com>
2024-01-01 18:35:09 +0100

    Using org file as SSOT

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $(dirname $0)
+[ ! -d ./emacs.d ] && emacs --batch -l org config.org -f org-babel-tangle
 setsid emacs --init-directory=./emacs.d $@
--- start_windows.bat
+++ start_windows.bat
@@ -1,2 +1,5 @@
 cd /d "%~dp0"
+
+if not exist "emacs.d\." emacs --batch -l org config.org -f org-babel-tangle
+
 start runemacs --init-directory=emacs.d
