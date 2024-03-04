9bcf23959c826736da2ddbfccdbd67bcd60222c6
Giulio Cocconi <coccogiulio8@gmail.com>
2024-03-04 21:29:44 +0100

    Added `nix-shell` automatic support

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
 [ ! -d ./emacs.d ] && emacs --batch -l org config.org -f org-babel-tangle
-setsid emacs --init-directory=./emacs.d $@
+
+setsid emacs --init-directory=./emacs.d $@ &
