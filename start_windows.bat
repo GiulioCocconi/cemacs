6588b14c26738606a63bbc7bb62396b0a97f36d6
Giulio Cocconi <coccogiulio8@gmail.com>
2024-04-12 10:27:38 +0200

    Fixed windows

--- start_windows.bat
+++ start_windows.bat
@@ -2,4 +2,4 @@ cd /d "%~dp0"
 
 if not exist "emacs.d\." emacs --batch -l org config.org -f org-babel-tangle
 
-start runemacs --init-directory=emacs.d
+start runemacs --init-directory=emacs.d --debug-init
