6a832f9b35ef63394447c45d27255b6c9e4df4e9
Giulio Cocconi <coccogiulio8@gmail.com>
2024-04-18 19:35:11 +0200

    Upgrade nix-shell logic (2)

--- start.sh
+++ start.sh
@@ -10,24 +10,31 @@ fi
 
 [ ! -d ./emacs.d ] && emacs --batch -l org config.org -f org-babel-tangle
 
-if [ -z "${NIX_EMACS}" ]; then
-    setsid $EMACS_CMD $@ &
-    exit
-fi
+if [ -n "{NIX_EMACS}" ]; then
 
+    search_for_shell_file() {
+	for path in "$@"; do
+	    if [ -e $path ]; then
+		SHELL_FILE=$path
+		return
+	    fi
+	done
+    }
 
-# TODO: Always check also cwd for nix-shells, but $1 should take precedence
+    if [[ $1 != "" ]]; then
+	search_for_shell_file "$(pwd)/shell.nix"\
+			      "$(pwd)/default.nix"
 
-if [[ ! $1 =~ ^.*\/(default|shell).nix ]]; then
-    # figure out the project root directory
-    ELISP_CMD="(message (get-vc-root \"$1\"))"
-    ROOT_PATH=$(emacs --batch --load ./emacs.d/early-init.el --eval "$ELISP_CMD" 2>&1 | tail -1)
-    echo $ROOT_PATH
+    elif [[ ! $1 =~ ^.*\/(default|shell).nix ]]; then
+	# figure out the project root directory
+	ELISP_CMD="(message (get-vc-root \"$1\"))"
+	ROOT_PATH=$(emacs --batch --load ./emacs.d/early-init.el\
+			  --eval "$ELISP_CMD" 2>&1 | tail -1)
 
-    if [ -e "$ROOT_PATH/shell.nix" ]; then
-        SHELL_FILE="$ROOT_PATH/shell.nix"
-    elif [ -e "$ROOT_PATH/default.nix" ]; then
-        SHELL_FILE="$ROOT_PATH/default.nix"
+	search_for_shell_file "$ROOT_PATH/shell.nix"\
+			      "$ROOT_PATH/default.nix"\
+			      "$(pwd)/shell.nix"\
+			      "$(pwd)/default.nix"
     fi
 fi
 
